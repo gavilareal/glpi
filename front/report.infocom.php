59be50c4daaad2deec31eabf68db34c83f87a5ca
Cédric Anne <cedric.anne@gmail.com>
2023-10-27 15:17:14 +0200

    Revert changes on getTableForItemType

--- front/report.infocom.php
+++ front/report.infocom.php
@@ -96,7 +96,7 @@ function display_infocoms_report($itemtype, $begin, $end)
 {
     global $DB, $valeurtot, $valeurnettetot, $valeurnettegraphtot, $valeurgraphtot, $CFG_GLPI, $stat, $chart_opts;
 
-    $itemtable = $itemtype::getTable();
+    $itemtable = getTableForItemType($itemtype);
    // report need name and ticket_tco, many asset type don't have it therefore are not compatible
     if (!$DB->fieldExists($itemtable, "ticket_tco", false)) {
         return false;
